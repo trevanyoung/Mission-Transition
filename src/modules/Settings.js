9c6c4e50257daaaa46758e435039813d3818c706
Trevan Young <trevan.young@gmail.com>
2019-04-18 14:56:52 -0500

    Changed the url file path in the settings file concerning the link to the API

--- src/modules/Settings.js
+++ src/modules/Settings.js
@@ -1,4 +1,5 @@
 export default {
     // remoteURL: "http://localhost:8088"
-       remoteURL: "https://git.heroku.com/mission-transition-api.git"
+    // remoteURL: "https://git.heroku.com/mission-transition-api.git"
+    remoteURL: "https://mission-transition-api.herokuapp.com"
 }
